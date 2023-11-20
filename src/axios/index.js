bd0c593efc0510419976da547009065200fd0c98
Mwngchao Wu <>
2023-11-20 17:36:03 +0800

    upd:url

--- src/axios/index.js
+++ src/axios/index.js
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import axios from "axios";
-const domain = process.env.REACT_APP_BASE_URL;
+const domain = "https://aimakerbackend.azurewebsites.net";
 const post = (url, params, custom_domain) => {
   let _domain = custom_domain || domain;
   return axios({

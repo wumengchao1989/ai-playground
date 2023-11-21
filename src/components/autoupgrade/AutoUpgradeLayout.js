bfbf6d1624d28f33176500beb07c9ed25fbed8d0
Mwngchao Wu <>
2023-11-21 08:14:29 +0800

    upd dfii content

--- src/components/autoupgrade/AutoUpgradeLayout.js
+++ src/components/autoupgrade/AutoUpgradeLayout.js
@@ -157,7 +157,7 @@ const AutoUpgradeLayout = () => {
               >
                 <ReactMarkdown
                   remarkPlugins={[[remarkGfm, { singleTilde: false }]]}
-                  children={originFileContent}
+                  children={fileContent}
                   components={{
                     code({ node, inline, className, children, ...props }) {
                       const match = /language-(\w+)/.exec(className || "");
